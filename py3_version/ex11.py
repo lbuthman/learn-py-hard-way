d85b215ca6b52a2e6e56d9ea85af57507c6d4d9b
lbuthman <lbuthman@gmail.com>
2017-04-20 17:55:51 -0500

    Add ex11, python3

--- py3_version/ex11.py
+++ py3_version/ex11.py
@@ -5,6 +5,6 @@ height = input()
 print("How much do you weight?"),
 weight = input()
 
-print("So you're %r years old, %r feet tall, and %r pounds heavy." % (
+print("So you're %r years old, %r feet tall, and %r heavy." % (
     age, height, weight
 ))
