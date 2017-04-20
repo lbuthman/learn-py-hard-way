e72afe7be5ad3e31055a2cff39e1e11131c9f301
lbuthman <lbuthman@gmail.com>
2017-04-20 05:42:18 -0500

    Add folder for exercises in Python 3

---
+++ py3_version/ex5.py
@@ -0,0 +1,15 @@
+my_name = "Luke C Buthman"
+my_age = 35
+my_height = 67      # inches
+my_weight = 170     # pounds
+my_eyes = "Blue"
+my_teeth = "White"
+my_hair = "Bald"
+
+print("Let's talk about %s." % my_name)
+print("He's %d inches tall." % my_height)
+print("And he's %d pounds heavy." % my_weight)
+print("He's got %s eyes and %s on top." % (my_eyes, my_hair))
+print("His teeth are usually %s, depending on the coffee." % my_teeth)
+print("If I add %d %d and %d, I get %d." % (my_age, my_height, my_weight,
+                                            my_age + my_height + my_weight))
