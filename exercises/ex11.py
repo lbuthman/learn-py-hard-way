7571174d528d1e0c3f2e319cb15eb9b39e2e4b36
lbuthman <lbuthman@gmail.com>
2017-04-20 17:53:26 -0500

    Add ex11, python2

---
+++ exercises/ex11.py
@@ -0,0 +1,10 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?", 
+weight = raw_input()
+
+print "So you're %r years old, %r feet tall, and %r pounds heavy." % (
+    age, height, weight
+)
---
+++ py3_version/ex12.py
@@ -0,0 +1,7 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print("So you're %r years old, %r feet tall, and %r pounds heavy." % (
+    age, height, weight
+))
