12fb7f50290c5131282f04d615923e8d067e27da
lbuthman <lbuthman@gmail.com>
2017-04-20 06:50:25 -0500

    Add ex7, python2

--- exercises/ex7.py
+++ exercises/ex7.py
@@ -1,6 +1,6 @@
 print "Mary had a little lamb."
 print "It's fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
+print "And everywhere that Mary went,"
 print "." * 10
 
 end1 = "C"
