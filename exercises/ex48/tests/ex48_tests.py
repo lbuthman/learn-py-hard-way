7713ec134e81f7f75c4da010be4080d23441d4f2
lbuthman <lbuthman@gmail.com>
2017-05-06 10:39:51 -0500

    Add ex49 tests for ex48 test file

--- exercises/ex48/tests/ex48_tests.py
+++ exercises/ex48/tests/ex48_tests.py
@@ -1,5 +1,6 @@
 from nose.tools import *
 from ex48 import lexicon
+from ex48 import parser
 
 
 def test_directions():
@@ -38,3 +39,62 @@ def test_errors():
     assert_equals(result, [("noun", "bear"),
                            ("error", "IAS"),
                            ("noun", "princess")])
+
+
+def test_peek():
+    word_list = lexicon.scan("bear eat beet")
+    assert_equal(parser.peek(word_list), "noun")
+    assert_equal(parser.peek(None), None)
+
+
+def test_match():
+    word_list = lexicon.scan("bear eat beet")
+    assert_equal(parser.match(word_list, 'noun'), ('noun', 'bear'))
+    assert_equal(parser.match(word_list, None), None)
+    assert_equal(parser.match(None, None), None)
+
+
+def test_skip():
+    word_list = lexicon.scan("the bear eat beet")
+    assert_equal(parser.skip(word_list, 'stop'), None)
+    assert_equal(parser.skip(None, 'stop'), None)
+
+
+def test_parse_verb():
+    word_list = lexicon.scan("and eat cats")
+    assert_equal(parser.parse_verb(word_list), ('verb', 'eat'))
+    bad_list = lexicon.scan("bears beets battle star galactica")
+    assert_raises(parser.ParserError, parser.parse_verb, bad_list)
+
+
+def test_parse_object():
+    word_list = lexicon.scan("the bear eat beet")
+    assert_equal(parser.parse_object(word_list), ('noun', 'bear'))
+    word_list = lexicon.scan("south beet")
+    assert_equal(parser.parse_object(word_list), ('direction', 'south'))
+    bad_list = lexicon.scan("eat beet")
+    assert_raises(parser.ParserError, parser.parse_object, bad_list)
+
+
+def test_parse_subject():
+    word_list = lexicon.scan("the bear eat beet")
+    assert_equal(parser.parse_subject(word_list), ('noun', 'bear'))
+    word_list = lexicon.scan("eat beet")
+    assert_equal(parser.parse_subject(word_list), ('noun', 'player'))
+    bad_list = lexicon.scan("identity theft is a crime jim")
+    assert_raises(parser.ParserError, parser.parse_subject, bad_list)
+
+
+def test_parse_sentence():
+    word_list = lexicon.scan("the bear eat beet")
+    sentence = parser.parse_sentence(word_list)
+    assert_equal(sentence.subject, 'bear')
+    assert_equal(sentence.verb, 'eat')
+    assert_equal(sentence.object, 'beet')
+    word_list = lexicon.scan("eat beet")
+    sentence = parser.parse_sentence(word_list)
+    assert_equal(sentence.subject, 'player')
+    assert_equal(sentence.verb, 'eat')
+    assert_equal(sentence.object, 'beet')
+    bad_list = lexicon.scan("bear beet eat the make sense none")
+    assert_raises(parser.ParserError, parser.parse_sentence, bad_list)
